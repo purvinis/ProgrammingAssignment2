b98a40d6bf04be01a37b72e8bccc79ba2be7036e
Purvinis <g.purvinis@gmail.com>
2020-08-06 16:41:47 -1000

    added example and used browser to understand

--- testing.R
+++ testing.R
@@ -1,25 +1,27 @@
 makeVector <- function(x = numeric()) {
       m <- NULL
       set <- function(y) {
-            x <<- y
+            x <<- y       #x will have the vector 2 3 4 5 from makeVector(c(2, 3, 4, 5))
             m <<- NULL
       }
+      #browser()
       get <- function() x
       setmean <- function(mean) m <<- mean
       getmean <- function() m
       list(set = set, get = get,
            setmean = setmean,
            getmean = getmean)
+      #browser()
 }
 
-makeVector(c(2,3,4,5))
+ans = makeVector(c(2,3,4,5))
 
 counter <- function() {   #function with no arguments
       x <- 0
       print(ls())    #ls returns string of objects in environment
       
       function() {
-            x <<- x+1  #defined in this env, but stored 
+            x <<- x+1  #defined in this env, but stored up/out
             x
       }
 }
