72723f46c93a947cf96bf131fdda5395c6b7b4ee
Purvinis <g.purvinis@gmail.com>
2020-08-07 09:33:35 -1000

    Added test code and additional comments. This version works

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 
 ## This function creates a "matrix" object that can cache its inverse.
 ## cacheSolve() uses this function
-## The function returns a list of functions
+## cacheSolve() needs a makeCacheMatrix object as its arguement
 
 makeCacheMatrix <- function(x = matrix()) {
       invOfx <- NULL                                 #initiallize the inverse
@@ -18,6 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
       list(set = set, get = get,
            setInv = setInv,
            getInv = getInv)
+      
 }
 
 
@@ -27,20 +28,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ##the inverse from the cache.
 
 cacheSolve <- function(x, ...) {   ## Return a matrix that is the inverse of 'x'
+      #x <- as.data.frame(x)
       invOfx <- x$getInv()
       if(!is.null(invOfx)) {
             message("getting cached inverse data")
             return(invOfx)
       }
+
       data <- x$get()
-      invOfX <- solve(data, ...)
-      x$setInv(m)
-      invOfX
+      invOfx <- solve(data, ...)
+      x$setInv(invOfx)
+      invOfx
 }
 
+#Code to test:
 n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
-n1$get()              # retrieve the value of matix n1
-n1$getInv()           # retrieve the value of n1, which should be NULL
-n1$set(matrix(c(1,2,3,4),nrow=2, ncol=2))          # reset value with a new n1
-cachemean(n1)          # notice new inverse
-n1$getInv()           # retrieve it directly, now that it has been cached
+specialm <- makeCacheMatrix(n1)
+cacheSolve(specialm)
+
+
+
